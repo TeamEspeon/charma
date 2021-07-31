21416d0ddd492281ef4009a99d31416d63bd531e
Paulo Choi <paulinho@hey.com>
2021-07-31 13:28:52 -0400

    Merge with CH-02 and push map + list view and navigation

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Charities from '../Charities';
 
 const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
     <Tab.Navigator>
-      {/* <Tab.Screen name="Home" component={HomeScreen} /> */}
+      <Tab.Screen name="Home" component={Charities} />
       {/* <Tab.Screen name="Settings" component={SettingsScreen} /> */}
       {/* <Tab.Screen name="Search" component={SettingsScreen} /> */}
     </Tab.Navigator>
