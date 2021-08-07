3ae3708c8391430c5ff17109d10c10e9150c8659
Sam Filip <samjfilip@gmail.com>
2021-08-07 12:56:01 -0700

    removed unused code from userProfile

--- Components/UserProfile.js
+++ Components/UserProfile.js
@@ -19,7 +19,6 @@ const UserProfile = ({ route, navigation }) => {
 
                 <View style={styles.infoContainer}>
                     <Text style={[styles.text, { fontWeight: "200", fontSize: 36 }]}>{user.firstName}</Text>
-                    {/* <Text style={[styles.text, { color: "#AEB5BC", fontSize: 14 }]}>Photographer</Text> */}
                 </View>
 
                 <View style={styles.statsContainer}>
@@ -36,24 +35,6 @@ const UserProfile = ({ route, navigation }) => {
                         <Text style={[styles.text, styles.subText]}>Following</Text>
                     </View>
                 </View>
-
-                {/* <View style={{ marginTop: 32 }}>
-                    <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-                        <View style={styles.mediaImageContainer}>
-                            <Image source={require("./assets/media1.jpg")} style={styles.image} resizeMode="cover"></Image>
-                        </View>
-                        <View style={styles.mediaImageContainer}>
-                            <Image source={require("./assets/media2.jpg")} style={styles.image} resizeMode="cover"></Image>
-                        </View>
-                        <View style={styles.mediaImageContainer}>
-                            <Image source={require("./assets/media3.jpg")} style={styles.image} resizeMode="cover"></Image>
-                        </View>
-                    </ScrollView>
-                    <View style={styles.mediaCount}>
-                        <Text style={[styles.text, { fontSize: 24, color: "#DFD8C8", fontWeight: "300" }]}>70</Text>
-                        <Text style={[styles.text, { fontSize: 12, color: "#DFD8C8", textTransform: "uppercase" }]}>Media</Text>
-                    </View>
-                </View> */}
                 <Text style={[styles.subText, styles.recent]}>Recent Activity</Text>
                 <View style={{ alignItems: "center" }}>
                     <View style={styles.recentItem}>
@@ -83,121 +64,121 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#FFF"
-},
-text: {
-    color: "#52575D"
-},
-image: {
-    flex: 1,
-    height: undefined,
-    width: undefined
-},
-titleBar: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginTop: 24,
-    marginHorizontal: 16
-},
-subText: {
-    fontSize: 12,
-    color: "#AEB5BC",
-    textTransform: "uppercase",
-    fontWeight: "500"
-},
-profileImage: {
-    width: 200,
-    height: 200,
-    borderRadius: 100,
-    overflow: "hidden"
-},
-dm: {
-    backgroundColor: "#41444B",
-    position: "absolute",
-    top: 20,
-    width: 40,
-    height: 40,
-    borderRadius: 20,
-    alignItems: "center",
-    justifyContent: "center"
-},
-active: {
-    backgroundColor: "#34FFB9",
-    position: "absolute",
-    bottom: 28,
-    left: 10,
-    padding: 4,
-    height: 20,
-    width: 20,
-    borderRadius: 10
-},
-add: {
-    backgroundColor: "#41444B",
-    position: "absolute",
-    bottom: 0,
-    right: 0,
-    width: 60,
-    height: 60,
-    borderRadius: 30,
-    alignItems: "center",
-    justifyContent: "center"
-},
-infoContainer: {
-    alignSelf: "center",
-    alignItems: "center",
-    marginTop: 16
-},
-statsContainer: {
-    flexDirection: "row",
-    alignSelf: "center",
-    marginTop: 32
-},
-statsBox: {
-    alignItems: "center",
-    flex: 1
-},
-mediaImageContainer: {
-    width: 180,
-    height: 200,
-    borderRadius: 12,
-    overflow: "hidden",
-    marginHorizontal: 10
-},
-mediaCount: {
-    backgroundColor: "#41444B",
-    position: "absolute",
-    top: "50%",
-    marginTop: -50,
-    marginLeft: 30,
-    width: 100,
-    height: 100,
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 12,
-    shadowColor: "rgba(0, 0, 0, 0.38)",
-    shadowOffset: { width: 0, height: 10 },
-    shadowRadius: 20,
-    shadowOpacity: 1
-},
-recent: {
-    marginLeft: 78,
-    marginTop: 32,
-    marginBottom: 6,
-    fontSize: 10
-},
-recentItem: {
-    flexDirection: "row",
-    alignItems: "flex-start",
-    marginBottom: 16
-},
-activityIndicator: {
-    backgroundColor: "#CABFAB",
-    padding: 4,
-    height: 12,
-    width: 12,
-    borderRadius: 6,
-    marginTop: 3,
-    marginRight: 20
-}
+  },
+  text: {
+      color: "#52575D"
+  },
+  image: {
+      flex: 1,
+      height: undefined,
+      width: undefined
+  },
+  titleBar: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      marginTop: 24,
+      marginHorizontal: 16
+  },
+  subText: {
+      fontSize: 12,
+      color: "#AEB5BC",
+      textTransform: "uppercase",
+      fontWeight: "500"
+  },
+  profileImage: {
+      width: 200,
+      height: 200,
+      borderRadius: 100,
+      overflow: "hidden"
+  },
+  dm: {
+      backgroundColor: "#41444B",
+      position: "absolute",
+      top: 20,
+      width: 40,
+      height: 40,
+      borderRadius: 20,
+      alignItems: "center",
+      justifyContent: "center"
+  },
+  active: {
+      backgroundColor: "#34FFB9",
+      position: "absolute",
+      bottom: 28,
+      left: 10,
+      padding: 4,
+      height: 20,
+      width: 20,
+      borderRadius: 10
+  },
+  add: {
+      backgroundColor: "#41444B",
+      position: "absolute",
+      bottom: 0,
+      right: 0,
+      width: 60,
+      height: 60,
+      borderRadius: 30,
+      alignItems: "center",
+      justifyContent: "center"
+  },
+  infoContainer: {
+      alignSelf: "center",
+      alignItems: "center",
+      marginTop: 16
+  },
+  statsContainer: {
+      flexDirection: "row",
+      alignSelf: "center",
+      marginTop: 32
+  },
+  statsBox: {
+      alignItems: "center",
+      flex: 1
+  },
+  mediaImageContainer: {
+      width: 180,
+      height: 200,
+      borderRadius: 12,
+      overflow: "hidden",
+      marginHorizontal: 10
+  },
+  mediaCount: {
+      backgroundColor: "#41444B",
+      position: "absolute",
+      top: "50%",
+      marginTop: -50,
+      marginLeft: 30,
+      width: 100,
+      height: 100,
+      alignItems: "center",
+      justifyContent: "center",
+      borderRadius: 12,
+      shadowColor: "rgba(0, 0, 0, 0.38)",
+      shadowOffset: { width: 0, height: 10 },
+      shadowRadius: 20,
+      shadowOpacity: 1
+  },
+  recent: {
+      marginLeft: 78,
+      marginTop: 32,
+      marginBottom: 6,
+      fontSize: 10
+  },
+  recentItem: {
+      flexDirection: "row",
+      alignItems: "flex-start",
+      marginBottom: 16
+  },
+  activityIndicator: {
+      backgroundColor: "#CABFAB",
+      padding: 4,
+      height: 12,
+      width: 12,
+      borderRadius: 6,
+      marginTop: 3,
+      marginRight: 20
+  }
 });
 
 export default UserProfile;
